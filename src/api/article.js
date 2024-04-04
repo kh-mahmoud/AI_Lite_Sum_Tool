db79dc552bb766b99a7bff32721f42fced2bd80e
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2024-04-04 14:16:15 +0100

    Update article.js

--- src/api/article.js
+++ src/api/article.js
@@ -11,7 +11,7 @@ export const Summurizer= async (url)=>
 {
 
    try {
-    const response = await axios.get(`https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=${encodeURIComponent(url)}&length=3&lang=${state.lang}`,
+    const response = await axios.get(`https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=${encodeURIComponent(url)}&length=3&lang=${state?.lang}`,
        {
         headers:{
           'X-RapidAPI-Key': RapidAPI_Key,
