c10c6cc1079662847d85afb21846eef3ca5f5d00
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2024-04-04 14:28:45 +0100

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -66,7 +66,6 @@ const Demo = () => {
       e.preventDefault()
       try {
          setisSummarize(true)
-         setArticle({ ...article, url: "", summary: "" })
          const { data } = await Summurizer(article.url)
 
          if (data?.summary) {
